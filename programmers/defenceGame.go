6f558af6d89ff18973074293481c63f79fb0e8b9
Harvey <kumyunsu@gmail.com>
2024-07-23 10:25:20 +0900

    defenceGame -> heap solution

---
+++ programmers/defenceGame.go
@@ -0,0 +1,8 @@
+package programmers
+
+func SolutionDefenceGame(n int, k int, enemy []int) int { //	7     3 	[4, 2, 4, 5, 3, 3, 1]	 return 5  =>최대 라운드
+
+	//끝까지 가다가 막혔을때 하나 써, (최댓값 찾아놓기.) -> 최댓값 만큼 인원수 += 해주고 또 진행.-> 반복. 및 횟수 계산. 찾을때 마다 정렬 해주는게 좋긴 할듯?
+
+	return 0
+}
