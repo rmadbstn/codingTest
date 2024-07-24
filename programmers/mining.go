b270ea1144208727ef9394eb09f059135014c325
Harvey <kumyunsu@gmail.com>
2024-07-24 14:58:26 +0900

    2024/07/24

---
+++ programmers/mining.go
@@ -0,0 +1,6 @@
+package programmers
+
+func SoulutionMining(picks []int, minerals []string) int { //곡괭이:[1, 3, 2]dia,iron,stone	광물 순서:["diamond", "diamond", "diamond", "iron", "iron", "diamond", "iron", "stone"]	return: 12
+
+	return 0
+}
